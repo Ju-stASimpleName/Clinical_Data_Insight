7b6ac7d0394d971a5813ae4c8302a1df8f53eb40
Ju_u <146108110+Ju-stASimpleName@users.noreply.github.com>
2024-02-08 00:59:12 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -604,7 +604,7 @@ elif selected_page == "Cancer du sein":
 elif selected_page == "Disclaimer":
 
     text_disclaimer = "Les prédictions générées par l'application ne peuvent en aucun cas se substituer à l'avis d'un professionnel de la santé.<br><br>Leur but est de fournir un support complémentaire lors du processus de prise de décision concernant d'éventuels examens complémentaires et traitements.<br><br>Il est crucial de souligner que les données que vous saisissez sont entièrement anonymisées et ne font l'objet d'aucune conservation, en stricte conformité avec les directives rigoureuses du Règlement Général de Protection des Données (RGPD)."
-    st.sidebar.markdown(f'''**(DISCLAIMER]**<br>{text_disclaimer}''', unsafe_allow_html=True)
+    st.sidebar.markdown(f'''**[DISCLAIMER]**<br>{text_disclaimer}''', unsafe_allow_html=True)
     st.header("DISCLAIMER")
     st.markdown(
         f"""
